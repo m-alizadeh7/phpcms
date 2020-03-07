73be38e420b11b6fd8f3c71b940abd8ab4cfbbab
Mehdi Adeli Far <mehdi_Adeli@aut.ac.ir>
2020-03-07 07:33:07 +0000

    fix writting bug

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include "inc/header.php" ?>
 <?php include "inc/navigation.php" ?>
-<?php include_once "classes/category_cls.php"?>
+<?php include_once "inc/classes/Categorys_cls.php"?>
 <!-- Page Content -->
 <div class="container">
 
